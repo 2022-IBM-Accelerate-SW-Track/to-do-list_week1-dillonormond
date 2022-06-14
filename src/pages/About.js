7a06ac4bc0b22c12c797e9aad158c15122398ead
unknown <dillonormond@gmail.com>
2022-06-14 11:59:12 -0600

    week 1 push

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import testimage from "../assets/test-image.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+            <div class="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src={testimage}
+                alt="Profile Pic"
+                ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Dillon Ormond!</div>
+              <div className="brief_description">
+                <p>
+
+                </p>
+              </div>
+            </div>
+          </div>
+  </div>
       </div>
     )
   }
